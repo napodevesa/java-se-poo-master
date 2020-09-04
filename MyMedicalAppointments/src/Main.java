117b718d53af882956ce2a7e194b01953afac2c6
ndev <napodevesa@gmail.com>
2020-09-04 10:58:31 +0200

    js3

--- MyMedicalAppointments/src/Main.java
+++ MyMedicalAppointments/src/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
 
         Doctor myDoctor = new Doctor("Anahí Salgado");
-        myDoctor.name = "Alejandro Rodríguez";
+        myDoctor.name = "Alex Rodríguez";
         myDoctor.showName();
 
     }
